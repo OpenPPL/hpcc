b5cbc5829d7d4c9b43137d75ed5bd7adb28f1d5d
ouguoyu <ouguoyu@sensetime.com>
2021-09-18 16:31:27 +0800

    add md5sum for hpcc_declare_pkg_dep in order to avoid duplicated downloading

--- cmake/hpcc-common.cmake
+++ cmake/hpcc-common.cmake
@@ -186,9 +186,10 @@ macro(hpcc_declare_git_dep dep_name git_url git_tag)
         UPDATE_DISCONNECTED True)
 endmacro()
 
-macro(hpcc_declare_pkg_dep dep_name pkg_url)
+macro(hpcc_declare_pkg_dep dep_name pkg_url pkg_md5)
     FetchContent_Declare(${dep_name}
         URL ${pkg_url}
+        URL_HASH MD5=${pkg_md5}
         SOURCE_DIR ${HPCC_DEPS_DIR}/${dep_name}
         BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/${dep_name}-build
         SUBBUILD_DIR ${HPCC_DEPS_DIR}/${dep_name}-subbuild
