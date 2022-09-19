cfacf2038919236676432b0756ab32377231d9cf
ouguoyu <ouguoyu@sensetime.com>
2022-09-19 17:21:28 +0800

    remove CUDA_NVCC_FLAGS

--- cmake/cuda-common.cmake
+++ cmake/cuda-common.cmake
@@ -4,7 +4,13 @@ else()
     set(__hpcc_cuda_common_INCLUDED__ TRUE)
 endif()
 
-set(CMAKE_CUDA_STANDARD 11)
+if(NOT DEFINED CMAKE_CUDA_STANDARD)
+    set(CMAKE_CUDA_STANDARD 11)
+    set(CMAKE_CUDA_STANDARD_REQUIRED ON)
+endif()
+
+set(CUDA_PROPAGATE_HOST_FLAGS OFF)
+find_package(CUDA REQUIRED) # required by CUDA_TOOLKIT_ROOT_DIR for cmake < 3.18
 
 # NOTE: cross compiling requires env `CUDA_TOOLKIT_ROOT`
 if(CUDA_TOOLKIT_ROOT_DIR AND NOT DEFINED ENV{CUDA_TOOLKIT_ROOT})
@@ -12,9 +18,6 @@ if(CUDA_TOOLKIT_ROOT_DIR AND NOT DEFINED ENV{CUDA_TOOLKIT_ROOT})
 elseif(DEFINED ENV{CUDA_TOOLKIT_ROOT} AND NOT CUDA_TOOLKIT_ROOT_DIR)
     set(CUDA_TOOLKIT_ROOT_DIR $ENV{CUDA_TOOLKIT_ROOT})
 endif()
-
-set(CUDA_PROPAGATE_HOST_FLAGS OFF)
-find_package(CUDA REQUIRED)
 
 # NOTE: `CMAKE_CUDA_COMPILER` and `CMAKE_CUDA_HOST_COMPILER` MUST be placed before enable_language(CUDA) for
 # recognizing arch and os
@@ -26,11 +29,11 @@ if(MSVC)
     enable_language(CUDA)
 
     if(HAVE_CXX_FLAG_UTF_8)
-        set(CUDA_NVCC_FLAGS "${CUDA_NVCC_FLAGS} -Xcompiler=/utf-8")
+        set(CMAKE_CUDA_FLAGS "${CMAKE_CUDA_FLAGS} -Xcompiler=/utf-8")
     endif()
-    set(CUDA_NVCC_FLAGS "${CUDA_NVCC_FLAGS} -Xcompiler=/wd4819,/wd4828")
-    set(CUDA_NVCC_FLAGS_DEBUG "-g -O0")
-    set(CUDA_NVCC_FLAGS_RELEASE "-O3")
+    set(CMAKE_CUDA_FLAGS "${CMAKE_CUDA_FLAGS} -Xcompiler=/wd4819,/wd4828")
+    set(CMAKE_CUDA_FLAGS_DEBUG "${CMAKE_CUDA_FLAGS_DEBUG} -g -O0")
+    set(CMAKE_CUDA_FLAGS_RELEASE "${CMAKE_CUDA_FLAGS_RELEASE} -O3")
 else()
     if(NOT CMAKE_CUDA_COMPILER)
         set(CMAKE_CUDA_COMPILER ${CUDA_TOOLKIT_ROOT_DIR}/bin/nvcc)
@@ -38,15 +41,14 @@ else()
     set(CMAKE_CUDA_HOST_COMPILER ${CMAKE_CXX_COMPILER})
     enable_language(CUDA)
 
-    set(CUDA_NVCC_FLAGS "${CUDA_NVCC_FLAGS} -Xcompiler=-fPIC,-Wall,-fvisibility=hidden")
-    set(CUDA_NVCC_FLAGS_DEBUG "-g")
-    set(CUDA_NVCC_FLAGS_RELEASE "-O3")
+    set(CMAKE_CUDA_FLAGS "${CMAKE_CUDA_FLAGS} -Xcompiler=-fPIC,-Wall,-fvisibility=hidden")
+    set(CMAKE_CUDA_FLAGS_DEBUG "${CMAKE_CUDA_FLAGS_DEBUG} -g")
+    set(CMAKE_CUDA_FLAGS_RELEASE "${CMAKE_CUDA_FLAGS_RELEASE} -O3")
 endif()
 
 if(CUDA_VERSION VERSION_GREATER_EQUAL "10.2")
     set(CMAKE_CUDA_FLAGS "${CMAKE_CUDA_FLAGS} -forward-unknown-to-host-compiler")
 endif()
-set(CMAKE_CUDA_FLAGS "${CMAKE_CUDA_FLAGS} ${CUDA_NVCC_FLAGS}")
 
 macro(hpcc_cuda_use_msvc_static_runtime)
     string(REPLACE -MD -MT CMAKE_CUDA_FLAGS_DEBUG "${CMAKE_CUDA_FLAGS_DEBUG}")
